e31d74b7a2b69cfa128eed2018545939a86fac7c
Adam Wielogórski <71398159+Khangineer@users.noreply.github.com>
2025-12-30 09:34:06 +0100

    93412302025

--- src/main/java/com/deathroll/DeathRoll/Controllers/GameController.java
+++ src/main/java/com/deathroll/DeathRoll/Controllers/GameController.java
@@ -1,11 +1,14 @@
 package com.deathroll.DeathRoll.Controllers;
 
-import com.deathroll.DeathRoll.DTOs.RollResponse;
+import com.deathroll.DeathRoll.DTOs.RollDTO;
+import com.deathroll.DeathRoll.Models.EntitiesMapper;
 import com.deathroll.DeathRoll.Models.Roll;
 import com.deathroll.DeathRoll.Models.User;
+import com.deathroll.DeathRoll.Repositories.RollRepository;
 import com.deathroll.DeathRoll.Repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
@@ -18,13 +21,19 @@ public class GameController {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private RollRepository rollRepository;
+
     @PostMapping("/PlaceRoll")
-    public ResponseEntity<RollResponse> Roll(
+    public ResponseEntity<RollDTO> Roll(
             @RequestBody Roll prevRoll,
-            Principal principal //Principal is automatically injected via Spring Principal resolver
+            Authentication authentication //Injected automatically via spring resolver
     ){
-        User user = userRepository.findByUsername(principal.getName()).orElseThrow(
-                () -> new RuntimeException("User not found")
-        );
+        User user = (User) authentication.getPrincipal();
+        Roll roll = new Roll(prevRoll.getRollBase());
+
+        roll.setUser(user);
+        rollRepository.save(roll);
+        return ResponseEntity.ok(EntitiesMapper.toRollDTO(roll));
     }
 }
